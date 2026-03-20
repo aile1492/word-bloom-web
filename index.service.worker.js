5395d49aa662f8c00882b4d3dfe6c08e9c4b4235
aile1492 <aile1492@users.noreply.github.com>
2026-03-20 16:56:35 +0900

    v2: Fix class_name conflicts, portrait layout, remove android duplicates from pck

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1773991828|9813148';
+const CACHE_VERSION = '1773993271|15435945';
 /** @type {string} */
 const CACHE_PREFIX = 'Word Bloom-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
